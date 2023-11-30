a35f7faafa81446d9d8fee53de47a860248435ad
soumya <soumyadeep.behala2001@gmail.com>
2023-11-30 22:23:05 +0530

    Minor changes

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const button = document.getElementById("click");
 const crush = document.getElementById("crush");
+const sec = document.getElementById("secret");
 
 button.addEventListener("click", function() {
     button.style.display = "none";
     let op = 0
+    let op2 = 0
     crush.style.opacity = op;
     crush.style.display = "block";
-    let int = window.setInterval(function() {
+    sec.style.display = "inline";
+    let int1 = window.setInterval(function() {
         op += 1;
         crush.style.opacity = op.toString() + "%";
-        console.log(op); 
-        if(op >= 100) clearInterval(int);
+        sec.style.opacity = op2.toString() + "%";
+        console.log("op = " + op); 
+        console.log("op2 = " + op2);
+        if(op >= 100) op2 += 5;
+        if(op2 >= 100) clearInterval(int1);
     }, 20);
 });
